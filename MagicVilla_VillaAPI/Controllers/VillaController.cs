7a248276b4d60c3295ae0266ba0b3efc1fb0f2fa
ksnvs <ks.sang1106@gmail.com>
2023-10-24 08:44:16 +0700

    Add project files.

---
+++ MagicVilla_VillaAPI/Controllers/VillaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        // GET: api/<VillaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<VillaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<VillaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<VillaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<VillaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        
+    }
+}
